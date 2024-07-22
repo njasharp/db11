75f3ad94e115aabd4a00c6bc760b9d3c1be09ba8
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-07-22 16:58:20 -0700

    Add files via upload

--- gdm.py
+++ gdm.py
@@ -88,7 +88,9 @@ if page == "Main Page":
     st.image("top_chart.PNG")
     st.subheader("Addtional Game Details List")
     st.dataframe(data1)
+    st.write("Top - themes in games")
     st.image("pic7.png")
+    st.write("Short list - hyper localized games")
     st.image("pic8.PNG")
         # Download button for the summary brief PDF
     st.download_button("Download PDF - short list", short_list_pdf, file_name="short_list.pdf")
